4cac0e151e2583c1203b057bb5310214d2cca7cf
Aman Chaudhary <armanachaudhary53@gmail.com>
2025-01-31 08:46:38 +0000

    Finish Security and passwords

--- blango/settings.py
+++ blango/settings.py
@@ -177,3 +177,12 @@ LOGGING = {
         "level": "DEBUG",
     },
 }
+
+
+#password_validation
+PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
